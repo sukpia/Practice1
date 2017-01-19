633e062ba88f5d1390a5f51375f90577a6ece5f5
v-suchon <v-suchon@MININT-689V1A8>
2017-01-19 10:25:41 -0800

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,75 @@
+﻿// 1/19/2017
+// I wrote this project to practice C#
+// All this codes are from msdn website:
+// https://msdn.microsoft.com/en-us/library/ms173153.aspx
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new BaseClass(); //bc is of type BaseClass, and its value is of type Baseclass. It can only access Method1.
+            DerivedClass dc = new DerivedClass(); //dc is of type DerivedClass, and its value if of type DerivedClass. It can access both Method1 and Method2.
+            BaseClass bcdc = new DerivedClass(); //bcdc is of type BaseClass, and its value is of type DerivedClass. It can only access Method1.
+
+            // The following 2 calls do what you would expect. They call the methods that are defined in BaseClass
+            bc.Method1();
+            bc.Method2();
+            // Output:
+            // Base - Method1
+            // Base - Method2
+
+            // The following 2 calls do what you would expect. They call the methods that are defined in DerivedClass
+            dc.Method1();
+            dc.Method2();
+            // Output:
+            // Derived - Method1
+            // Derived - Method2
+
+            // The following 2 calls produce different results, depending on whether override (Method1) or new (Method2) is used.
+            bcdc.Method1();
+            bcdc.Method2();
+            // Output:
+            // Derived - Method1
+            // Base - Method2
+
+            Console.Read();
+        }
+    }
+
+    class BaseClass
+    {
+        public virtual void Method1()
+        {
+            Console.WriteLine("Base - Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Base - Method2");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        // Add new to the definition of Method2 to to hide the inherited BaseClass Method2
+        public new void Method2()
+        {
+            Console.WriteLine("Derived - Method2");
+        }
+
+        // the use of override modifier enables bcdc access the Method1 in DerivedClass. In order to use the override modifier, u need to put virtual modifier in BaseClass Method1
+        // use override to extend the base class method.
+        public override void Method1()
+        {
+            Console.WriteLine("Derived - Method1");
+        }
+    }
+}
---
+++ Practice1a/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare objects of the derived classes and test which version of ShowDetails is run, base or derived.
+            TestCars1();
+
+            //Declare objects of the base classes, instantiated with the derived classes, and repeat the tests.
+            TestCars2();
+
+            // Declare objects of the derived classes and call ShowDetails directly.
+            TestCars3();
+
+            // Declare objets of the base class, instantiated with the derived classes, and repeat the tests.
+            TestCars4();
+
+            Console.Read();
+        }
+
+        public static void TestCars1()
+        {
+            Console.WriteLine("\nTestCars1");
+            Console.WriteLine("-----------");
+
+            Car car1 = new Car();
+            car1.DescribeCar();
+            Console.WriteLine("-----------");
+
+            // Notice the output from this test case. The new modifier is
+            // used in the definition of ShowDetails in the ConvertibleCar class.
+            ConvertibleCar car2 = new ConvertibleCar();
+            car2.DescribeCar();
+            Console.WriteLine("-----------");
+
+            Minivan car3 = new Minivan();
+            car3.DescribeCar();
+            Console.WriteLine("-----------");
+        }
+        // Output:  
+        // TestCars1  
+        // ----------  
+        // Four wheels and an engine.  
+        // Standard transportation.  
+        // ----------  
+        // Four wheels and an engine.  
+        // Standard transportation.  
+        // ----------  
+        // Four wheels and an engine.  
+        // Carries seven people.  
+        // ----------  
+
+        public static void TestCars2()
+        {
+            Console.WriteLine("\nTestCars2");
+            Console.WriteLine("-----------");
+
+            var cars = new List<Car> { new Car(), new ConvertibleCar(), new Minivan() };
+
+            foreach (var car in cars)
+            {
+                car.DescribeCar();
+                Console.WriteLine("-----------");
+            }
+        }
+        // Output:  
+        // TestCars2  
+        // ----------  
+        // Four wheels and an engine.  
+        // Standard transportation.  
+        // ----------  
+        // Four wheels and an engine.  
+        // Standard transportation.  
+        // ----------  
+        // Four wheels and an engine.  
+        // Carries seven people.  
+        // ----------  
+
+        public static void TestCars3()
+        {
+            Console.WriteLine("\nTestCars3");
+            Console.WriteLine("-----------");
+            ConvertibleCar car2 = new ConvertibleCar();
+            Minivan car3 = new Minivan();
+            car2.ShowDetails();
+            car3.ShowDetails();
+        }
+        // Output:  
+        // TestCars3  
+        // ----------  
+        // A roof that opens up.  
+        // Carries seven people.
+
+        public static void TestCars4()
+        {
+            Console.WriteLine("\nTestCars4");
+            Console.WriteLine("-----------");
+            Car car2 = new ConvertibleCar();
+            Car car3 = new Minivan();
+            car2.ShowDetails();
+            car3.ShowDetails();
+        }
+        // Output:  
+        // TestCars4  
+        // ----------  
+        // Standard transportation.  
+        // Carries seven people.
+    }
+
+    // Define the base class, Car. The class defines 2 methods,
+    // DescribeCar and ShowDetails. DescribeCar calls ShowDetails, and each derived class also defines a ShowDetails method.
+    // The example tests which version of ShowDetails is selected. the base class method or the derived class method.
+    class Car
+    {
+        public void DescribeCar()
+        {
+            Console.WriteLine("Four wheels and an engine.");
+            ShowDetails();
+        }
+
+        public virtual void ShowDetails()
+        {
+            Console.WriteLine("Standard transportation.");
+        }
+    }
+
+    // Define the derived classes.
+
+    // Class ConvertibleCar uses the new modifier to acknowledge that ShowDetails hides the base class method.
+    class ConvertibleCar : Car
+    {
+        public new void ShowDetails()
+        {
+            Console.WriteLine("A roof that opens up.");
+        }
+    }
+
+    // Class Minivan uses the override modifier to specify that ShowDetails extends the base class method.
+    class Minivan : Car
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Carries seven people.");
+        }
+    }
+}
